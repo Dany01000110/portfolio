05b8281a424c72467487fa148f2dad4d275ad6c6
Dany01000110 <danette41240mc@gmail.com>
2021-03-31 02:17:35 +0200

    Nothing

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <meta name="twitter:title" content="Dany.01000110 | Mon portfolio">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
-    <link rel="stylesheet" href="assets/css/styles.min.css?h=4ac31c4ea98628f60401606a6812327b">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" integrity="sha512-RXf+QSDCUQs5uwRKaDoXt55jygZZm2V++WUZduaU/Ui/9EGp3f/2KZVahFZBKGH0s774sd3HmrhUy+SgOFQLVQ==" crossorigin="anonymous"></script>
 </head>
 
