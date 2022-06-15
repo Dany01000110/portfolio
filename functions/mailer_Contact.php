b7a6693d4b374bf3060e2554fe48f0061605176c
Dany01000110 <danette41240mc@gmail.com>
2022-06-16 00:50:53 +0200

    Fix: Tentative de protéger le formulaire

--- functions/mailer_Contact.php
+++ functions/mailer_Contact.php
@@ -6,6 +6,9 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
+$postreplace1 = str_replace("<", "<\'", $_POST["question1"]);
+$postreplace2 = str_replace(">", " ", $postreplace1);
+
 $mail = new PHPMailer();
 $mail->IsSMTP();
 $mail->Host = 'bonite.o2switch.net';                //Adresse IP ou DNS du serveur SMTP
@@ -21,7 +24,7 @@ if($mail->SMTPAuth){
 $mail->CharSet = 'UTF-8';                           //Encodage des caractères
 $mail->smtpConnect();                               //Connexion au serveur SMTP
 $mail->From = 'contacts@dany01000110.xyz';         //Adresse email de l'expéditeur
-$mail->FromName = 'Prise de contact'; //Nom de l'expéditeur
+$mail->FromName = 'Moi'; //Nom de l'expéditeur
 
 $mail->Subject = 'Prise de contact - '.$_POST['name'];                      //Le sujet du mail
 $mail->WordWrap = 50; 			                   //Nombre de caracteres pour le retour a la ligne automatique
@@ -36,12 +39,12 @@ $mail->MsgHTML('<style>
     <div style="width:calc(100% - 20px);margin:20px auto;padding:10px;border-radius:2.5px;background-color:rgb(231, 231, 231);filter: drop-shadow(0px 0px 5px rgba(169, 173, 175, 0.42));">
         <div style="font-size:1.2em;font-weight:800;user-select:none;text-align:cente;">Détail de la prise de contact :</div>
         <div style="font-size:.9em;font-weight:500;user-select:text;"><span style="font-weight:600;user-select:none;">Nom :</span> '.$_POST['name'].'</div>
-        <div style="font-size:.9em;font-weight:500;user-select:text;"><span style="font-weight:600;user-select:none;">Adresse mail :</span> '.$_POST['email'].'</div>
-        <div style="font-size:.9em;font-weight:500;user-select:text;"><span style="font-weight:600;user-select:none;">Message :</span> '.$_POST['message'].'</div>
+        <div style="font-size:.9em;font-weight:500;user-select:text;"><span style="font-weight:600;user-select:none;">Adresse mail :</span> <a href="mailto:'.$_POST['email'].'" target="_blank">'.$_POST['email'].'</a></div>
+        <div style="font-size:.9em;font-weight:500;user-select:text;"><span style="font-weight:600;user-select:none;">Message :</span> '.$postreplace2.'</div>
     </div>
 </div>'); 		                //Le contenu au format HTML
 $mail->IsHTML(true);
-$mail->AddAddress('contacts@dany01000110.xyz','...'); //Destinataire
+$mail->AddAddress('contacts@dany01000110.xyz','Moi'); //Destinataire
 if (!$mail->send()) {
     header('Location: ../?errormail='.$mail->ErrorInfo);
 } else{
